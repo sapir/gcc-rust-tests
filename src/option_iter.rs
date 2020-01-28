6051649654c36c20f2e9eea4538a569f7a6cd76e
sapir <yasapir@gmail.com>
2020-01-28 02:43:53 +0200

    Add enum-with-nice test to option_iter test

--- src/option_iter.rs
+++ src/option_iter.rs
@@ -1,7 +1,16 @@
-pub fn test() -> i32 {
+pub fn test_just_an_int() -> i32 {
     let mut total = 0;
-    for i in Some(5) {
-        total += i;
+    for (_, i) in Some((8, &5)) {
+        total += *i;
+    }
+    total
+}
+
+// None should occupy a niche
+pub fn test_tuple_with_ref() -> i32 {
+    let mut total = 0;
+    for (_, i) in Some((8, &5)) {
+        total += *i;
     }
     total
 }
