9a62481d6d7cf369880d878458736aebb3bf67cc
sapir <yasapir@gmail.com>
2020-07-15 18:30:36 +0300

    Add more subtests to std_fold

--- src/std_fold.rs
+++ src/std_fold.rs
@@ -1,3 +1,11 @@
-pub fn test() -> i32 {
-    (0..9).into_iter().fold(0, |a, b| a + b)
+pub fn test_fold_start_value() -> i32 {
+    (0..9).fold(20, |a, _b| a)
+}
+
+pub fn test_fold_end_value() -> i32 {
+    (0..9).fold(20, |_a, b| b)
+}
+
+pub fn test_sum_by_fold() -> i32 {
+    (0..9).fold(0, |a, b| a + b)
 }
